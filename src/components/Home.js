df33ef35f02d71d67b63ed923ed6f6ff39ee308f
Ivan Medina <medinadiazivan@gmail.com>
2017-07-06 23:51:45 -0500

    Login && Signup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import {
-  Text
+	Button,
+	StyleSheet,
+	ToastAndroid,
+	View
 } from 'react-native';
 
+import { Actions } from 'react-native-router-flux';
+
 export default class Home extends Component {
+	goToLogin() {
+		Actions.login();
+	}
+
+	goToSignup() {
+		Actions.signup();
+	}
+
 	render() {
 		return (
-			<Text>Home</Text>
+			<View style={styles.container}>
+				<Button
+					onPress={this.goToLogin}
+				  title="Iniciar Sesión"
+				  color="#2196F3"
+				  accessibilityLabel="Iniciar Sesión"
+				/>
+				<Button
+					onPress={this.goToSignup}
+				  title="Regístrate"
+				  color="#841584"
+				  accessibilityLabel="Regístrate"
+				/>
+			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
---
+++ src/components/Signup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { Text } from 'react-native';
+
+export default class Signup extends Component {
+	render() {
+		return (
+			<Text>Register</Text>
+		)
+	}
+}
